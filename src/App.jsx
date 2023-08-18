5606f854388e1a28cc9823ef9aac60ec52d98ac4
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-18 09:55:11 +0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
+import Header from './components/layout/Header';
 
 export default function App() {
-  const [activePage, setActivePage] = useState(2);
-
+  let activePage = 1;
   return (
     <div>
-      <div className="pb-4">
-        <button
-          onClick={() => setActivePage(1)}
-          className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
-        >
-          Home
-        </button>
-        <button
-          onClick={() => setActivePage(2)}
-          className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
-        >
-          About
-        </button>
-      </div>
+      <Header />
       {activePage === 1 && <HomePage />}
       {activePage === 2 && <AboutPage />}
     </div>
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+export default function Header() {
+  const [activePage, setActivePage] = useState(2);
+  return (
+    <div className="pb-4">
+      <button
+        onClick={() => setActivePage(1)}
+        className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
+      >
+        Home
+      </button>
+      <button
+        onClick={() => setActivePage(2)}
+        className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
+      >
+        About
+      </button>
+    </div>
+  );
+}
