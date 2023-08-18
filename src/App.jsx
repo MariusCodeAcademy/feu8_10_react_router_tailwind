ad1555ff2a2444001bb06336a916505696392bbb
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-18 09:51:06 +0300

    activePage router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 
 export default function App() {
+  const [activePage, setActivePage] = useState(2);
+
   return (
     <div>
-      <HomePage />
+      <div className="pb-4">
+        <button
+          onClick={() => setActivePage(1)}
+          className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
+        >
+          Home
+        </button>
+        <button
+          onClick={() => setActivePage(2)}
+          className="border hover:bg-slate-200 border-black rounded-sm px-3 py-1"
+        >
+          About
+        </button>
+      </div>
+      {activePage === 1 && <HomePage />}
+      {activePage === 2 && <AboutPage />}
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline">Welcome to Home page</h1>
+      <h1 className="text-3xl font-bold underline pb-4">
+        Welcome to Home page
+      </h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
         numquam, hic, architecto error non optio incidunt facere voluptatum,
